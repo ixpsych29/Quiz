e51e8576e30aeec6ff730c64891af91581549148
Abdullah <abdullahbinrafique945@gmail.com>
2022-05-23 00:34:55 +0500

    adding pictures to complete the Learning Book

--- app/src/main/java/com/example/learningappwithquizz/MainActivity.java
+++ app/src/main/java/com/example/learningappwithquizz/MainActivity.java
@@ -11,12 +11,19 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button b1, b2;
+    Button a, b, c, d, e, f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b1=(Button) findViewById(R.id.learnBTN);
         b2=(Button) findViewById(R.id.quizBTN);
+        a = (Button)findViewById(R.id.Abtn);
+        b = (Button)findViewById(R.id.Bbtn);
+        c = (Button)findViewById(R.id.Cbtn);
+        d = (Button)findViewById(R.id.Dbtn);
+        e = (Button)findViewById(R.id.Ebtn);
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +36,41 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent in2 = new Intent(MainActivity.this,Quiz.class);
                 startActivity(in2);
+            }
+        });
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in1 = new Intent(MainActivity.this, AlphaA.class);
+                startActivity(in1);
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in1 = new Intent(MainActivity.this, AlphaB.class);
+                startActivity(in1);
+            }
+        });
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in1 = new Intent(MainActivity.this, AlphaC.class);
+                startActivity(in1);
+            }
+        });
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in1 = new Intent(MainActivity.this, AlphaD.class);
+                startActivity(in1);
+            }
+        });
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in1 = new Intent(MainActivity.this, AlphaE.class);
+                startActivity(in1);
             }
         });
     }
