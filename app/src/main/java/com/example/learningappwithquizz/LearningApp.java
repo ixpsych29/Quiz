4402c11261218a19de2f0628798d4762c9425d49
Abdullah <abdullahbinrafique945@gmail.com>
2022-05-22 14:48:48 +0500

    After Some Changes and adding more functionality

--- app/src/main/java/com/example/learningappwithquizz/LearningApp.java
+++ app/src/main/java/com/example/learningappwithquizz/LearningApp.java
@@ -1,4 +1,11 @@
 package com.example.learningappwithquizz;
 
-public class LearningApp {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LearningApp extends Activity{
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.learningapp);
+    }
 }
--- app/src/main/java/com/example/learningappwithquizz/MainActivity.java
+++ app/src/main/java/com/example/learningappwithquizz/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.learningappwithquizz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1, b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=(Button) findViewById(R.id.learnBTN);
+        b2=(Button) findViewById(R.id.quizBTN);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in1 = new Intent(MainActivity.this, LearningApp.class);
+                startActivity(in1);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in2 = new Intent(MainActivity.this,Quiz.class);
+                startActivity(in2);
+            }
+        });
     }
+
 }
